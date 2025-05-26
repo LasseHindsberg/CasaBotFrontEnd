616d1bc25403c3c733064475f6ea90b0e27af95b
Fr0styy <lassehindsberg@gmail.com>
2025-05-26 13:34:08 +0200

    simplified function

--- CasaBot-Vue/src/components/FaqOptions.vue
+++ CasaBot-Vue/src/components/FaqOptions.vue
@@ -18,16 +18,16 @@ function handleClick(question) {
 </script>
 
 <template>
-    <div class="mb-4 w-full max-w-2xl bg-gray-500/40 rounded-lg shadow-md border border-gray-200 p-4">
+    <div class="mb-4 w-full max-w-5xl bg-gray-500/40 rounded-lg shadow-md border border-gray-200 p-4">
         <div class="grid grid-cols-1 sm:grid-cols-2 gap-2">
             <button
-                v-for="(question, index) in faqQuestions"
-                :key="index"
-                @click="() => handleClick(question)"
-                class="px-3 py-2 bg-gray-800 hover:bg-gray-700 rounded-md text-sm text-white border border-gray-600 transition w-full"
-            >
-                {{ question }}
-            </button>
+    v-for="(question, index) in faqQuestions"
+    :key="index"
+    @click="handleClick(question)"
+    class="px-3 py-2 bg-gray-800 hover:bg-gray-700 rounded-md text-sm text-white border border-gray-600 transition w-full"
+>
+    {{ question }}
+</button>
         </div>
     </div>
 </template>
