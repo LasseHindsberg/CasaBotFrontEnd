714c7a87a00e86fbbd70817cbeea5cfafcb0f203
Fr0styy <lassehindsberg@gmail.com>
2025-06-22 16:04:21 +0200

    vercel test

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 // Set base to '/CasaBotFrontEnd/' for GitHub Pages project page deployment
 export default defineConfig({
-  base: '/CasaBotFrontEnd/',
+  // base: '/CasaBotFrontEnd/', - testing using vercel for deployment
   plugins: [
     vue(),
     tailwindcss()
